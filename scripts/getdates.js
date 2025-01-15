9c725d004b77dc76540f9534c11df9b3bdac9c47
chukwunonso <onyewuchichukwunonso@gmail.com>
2025-01-15 18:57:23 +0100

    week1

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,9 +1,2 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Get the current year
-    const currentYear = new Date().getFullYear();
-    document.getElementById("currentyear").textContent = currentYear;
-
-    // Get the last modified date
-    const lastModifiedDate = document.lastModified;
-    document.getElementById("lastModified").textContent = `Last modified: ${lastModifiedDate}`;
-});
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
